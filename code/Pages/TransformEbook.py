df32e1258526b8d9b726f85278581f77abe36790
bsplu <ruc.bs.plu@hotmail.com>
2018-04-20 01:25:37 +0200

    估计得用session

--- code/Pages/TransformEbook.py
+++ code/Pages/TransformEbook.py
@@ -1,7 +1,7 @@
 #---------------------------------
 from config import *
 from main  import *
-from flask import Flask, render_template, request, redirect,  url_for
+from flask import Flask, render_template, request, redirect,  url_for, g
 from flask.ext.babel import gettext
 #---------------------------------
 # 上传文件
@@ -24,24 +24,42 @@ class UploadForm(FlaskForm):
 
 
     def validate_file(self, field):
-
+        print("file check")
         str_filename = field.data.filename
         if not ('.' in str_filename and \
            str_filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS):
+           print("格式不对")
            raise ValidationError(gettext('文件格式不对'))
 
 class TransformForm(FlaskForm):
+    filename = ""
+    saveFileName = ""
+    filePath = ""
+
     transform = SubmitField('transform')
+
+    # def validate_transform(self, field):
+    #     if(self.filename =="" or self.saveFileName =="" or self.filePath == ""):
+    #         raise ValidationError(gettext('请先上传文件'))
 
 @app.route('/TransformEbook' , methods = ['GET', 'POST']  )
 def TransformEbook():
     #....
 
+    
     form = UploadForm()
+
+
+
     formTran = TransformForm()
+ 
+        
 
     if form.validate_on_submit():
+        print("-------------------------")
         if(form.upload.data):
+            print("-------------------------")
+
             filename = form.file.data.filename
             # secureFilename = secure_filename(filename)
             saveFileName = str(time.time()) + '-' + request.environ['REMOTE_ADDR']+'.txt'
@@ -54,14 +72,18 @@ def TransformEbook():
                 os.makedirs(filePath) 
             form.file.data.save(os.path.join(filePath , saveFileName))
             # 准备转化
-            print("-------------------------")
-            print(filename)
+            formTran.filename = filename
+            formTran.saveFileName = saveFileName
+            formTran.filePath = filePath
         else:
             print("unknown submit")
+        
     
     if formTran.validate_on_submit():
         if(formTran.transform.data):
             print("转化")
+            print(formTran.filePath)
+            init_project(formTran.filePath, formTran.filename)
         # return redirect(url_for('TransformEbook'))
 
     return render_template('TransformEbook.html.j2', app = app, form=form, formTran=formTran)
--- code/static/Scripts/TransformEbook.js
+++ code/static/Scripts/TransformEbook.js
@@ -1,39 +1,37 @@
 
 $(document).ready(function () { 
-    console.log("准备");
-    $("form#formUpload").on("submit",function(event){
-        $(".progress").css("display","block");    //显示进度条
-        event.preventDefault();    //不是很懂这里是干嘛的，原文说是为了阻止表单提交
-        var formData = new FormData(this);
-        formData.append("upload", true);
-
-　　　　 //如果需要为表单添加一些其他字段的数据可以调用formData.append('key','value')来实现
-        //开始用ajax上传文件
-        $.ajax({
-            xhr : function(){
-                var xhr = new XMLHttpRequest();
-                xhr.upload.addEventListener('progress' ,function(e){
-                    if (e.lengthComputable){
-                        var percent = Math.round(e.loaded * 100. / e.total);
-                        $(".progress-bar").attr("aria-valuenow",percent).text(percent+"%").css("width",percent+"%");
-                    }
-                });
-                return xhr;
-            },
-            type : 'POST',
-            url : '/TransformEbook',
-            cache : false,
-            data : formData,
-            processData : false,
-            //这条主要是指出了jQuery不要去处理发送的数据
-            contentType : false})
-            //这里是说明不要ajax去设置Content-Type请求头，原因我也不懂。。
-            .done(    //接在整个ajax请求方法后面，表示处理完成或失败时调用的函数
-function(){
-    console.log('success');
-}).fail(
-function(){alert('failed');});
-            
-
-    });
+//     console.log("准备");
+//     $("form#formUpload").on("submit",function(event){
+//         $(".progress").css("display","block");    //显示进度条
+        
+//         var formData = new FormData(this);
+//         formData.append("upload", true);
+        
+// 　　　　 //如果需要为表单添加一些其他字段的数据可以调用formData.append('key','value')来实现
+//         //开始用ajax上传文件
+//         $.ajax({
+//             xhr : function(){
+//                 var xhr = new XMLHttpRequest();
+//                 xhr.upload.addEventListener('progress' ,function(e){
+//                     if (e.lengthComputable){
+//                         var percent = Math.round(e.loaded * 100. / e.total);
+//                         $(".progress-bar").attr("aria-valuenow",percent).text(percent+"%").css("width",percent+"%");
+//                     }
+//                 });
+//                 return xhr;
+//             },
+//             type : 'POST',
+//             url : '/TransformEbook',
+//             cache : false,
+//             data : formData,
+//             processData : false,
+//             //这条主要是指出了jQuery不要去处理发送的数据
+//             contentType : false})
+//             //这里是说明不要ajax去设置Content-Type请求头，原因我也不懂。。
+//             .done(    //接在整个ajax请求方法后面，表示处理完成或失败时调用的函数
+// function(){
+//     console.log('success');
+// }).fail(
+// function(){ console.log( 'false' );});
+//     });
 })
