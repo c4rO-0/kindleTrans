af892e93f231745643a662e66deedff8156793dc
bsplu <ruc.bs.plu@hotmail.com>
2018-05-10 10:06:02 +0200

    获取目录

--- code/serverside/Script_sync.py
+++ code/serverside/Script_sync.py
@@ -2,3 +2,10 @@
 # -*- coding: utf-8 -*-
 #---------------------------------
 # 备份上传的小说
+
+import os
+
+# 读取文件夹
+dirs = os.listdir(os.path.abspath(os.path.join(os.path.dirname(__file__),'..','uploads')) )
+for file in dirs:
+   print(file)
