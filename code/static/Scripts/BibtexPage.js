27821dc25697b733cd2e2fbd6c6ba65e7756341a
Sheng Bi <shengbi@fhi-berlin.mpg.de>
2020-03-30 11:13:14 +0200

    black and white (P.W. Anderson)

--- code/static/Scripts/BibtexPage.js
+++ code/static/Scripts/BibtexPage.js
@@ -988,6 +988,14 @@ $(document).ready(function () {
 
     })
 
+    var body = document.body;
+    body.style['filter'] = 'progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)';
+    if (!body.style['filter']) {
+        body.style['-webkit-filter'] = 'grayscale(1)';
+        body.style['filter'] = 'grayscale(1)';
+    }
+
+
 })
 
 
