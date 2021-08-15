46e2698b2e577eafbd1fdd65edc7031f9d438741
bsplu <ruc.bs.plu@hotmail.com>
2021-08-15 11:04:46 +0800

    add:kill option

--- code/serverside/Script_start.sh
+++ code/serverside/Script_start.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# quit screen
-screen -S kindle -X quit;
 
-# create screen and run
-kindleRoot=$( dirname $( dirname $(dirname "$0") ) )
+if [ "$1" == "-h" ]; then
+	echo '| bash Script_start.sh [-k]'
+	echo '|     : start to run kindle.'
+	echo '| -k  : kill kindle'
+	exit 0
+elif [ "$1" == "-k" ]; then
+    # quit screen
+    screen -S kindle -X quit;
+else
+    # quit screen
+    screen -S kindle -X quit;
 
-screen -S kindle -dm bash -c "cd $kindleRoot; deactivate; source */bin/activate;clear; cd code; python main.py >> ./log/flask.log 2>&1"
+    # create screen and run
+    kindleRoot=$( dirname $( dirname $(dirname "$0") ) )
+
+    screen -S kindle -dm bash -c "cd $kindleRoot; deactivate; source */bin/activate;clear; cd code; python main.py >> ./log/flask.log 2>&1"
+fi
